c4482e0234db58ad3ba2dad38d367a9133b08174
Sailesh Bajracharya <nhuja08@gmail.com>
2016-08-17 17:28:39 +0545

    add moment file

---
+++ moment-example.js
@@ -0,0 +1,15 @@
+var moment = require('moment');
+var now = moment();
+
+
+console.log(now.format());
+
+console.log(now.format('X'));
+console.log(now.valueOf());
+
+
+var timestamp = 1471433766852;
+var timestampmoment = moment.utc(timestamp);
+console.log(timestampmoment.local().format('LT'));
+
+//console.log(now.format( 'MMM Do YYYY, '+'LT'));
